88424944dba73b40988b56866b88c8b7d53e8d93
xoma_star <sasha.sirotyin09@gmail.com>
2022-06-06 21:41:33 +0500

    aboba

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const SERVER_LOCATION = process.env.NODE_ENV === 'development' ? 'http://192.168.1.24:5000/' : 'https://radio-322.herokuapp.com'
+export const SERVER_LOCATION = process.env.NODE_ENV === 'development' ? 'http://192.168.1.24:5000/' : 'https://radio-322.herokuapp.com/'
 export const TRACK_DATA_LOCATION = SERVER_LOCATION + 'tracks/'
 export const TRACK_LOCATION = SERVER_LOCATION + '/audio/'
 export const COVER_LOCATION = SERVER_LOCATION + '/covers/'
