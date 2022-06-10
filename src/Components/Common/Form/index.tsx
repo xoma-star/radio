85818b1640a355bf57cb8304b54d97ac6a90cd80
xoma_star <sasha.sirotyin09@gmail.com>
2022-06-10 10:02:55 +0500

    form fix

--- src/Components/Common/Form/index.tsx
+++ src/Components/Common/Form/index.tsx
@@ -5,17 +5,18 @@ import {win_install} from "../../../Images/Arts";
 interface props{
     children?: ReactNode,
     header?: string,
-    description?: string
+    description?: string,
+    showArt?: boolean
 }
 
-const Form = ({children, header, description}: props) => {
+const Form = ({children, header, description, showArt = false}: props) => {
     return <div className={'form-wrapper'}>
-        <div className={'form-art'}>
-            <img src={win_install}/>
-        </div>
+        {showArt && <div className={'form-art'}>
+            <img alt={'art'} src={win_install}/>
+        </div>}
         <div className={'form'}>
-            <div className={'form-header'}>{header}</div>
-            <div className={'form-description'}>{description}</div>
+            {header && <div className={'form-header'}>{header}</div>}
+            {description && <div className={'form-description'}>{description}</div>}
             <div className={'form-fields'}>{children}</div>
         </div>
     </div>
